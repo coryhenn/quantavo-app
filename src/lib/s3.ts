469b6b5c25e8b5dbf493e15fa6de52194eed664c
coryhenn <coryhenn34@gmail.com>
2025-08-16 06:57:29 -0600

    Fixed some s3 routing issues in s3.ts

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -5,7 +5,7 @@ export function makeS3() {
   const region = process.env.SUPABASE_S3_REGION || "us-east-1";
   return new S3Client({
     region,
-    endpoint: `https://${ref}.supabase.co/storage/v1/s3`,
+    endpoint: `https://${process.env.SUPABASE_PROJECT_REF}.supabase.co/storage/v1/s3`,
     forcePathStyle: true, // required for Supabase S3 gateway
     credentials: {
       accessKeyId: process.env.SUPABASE_S3_ACCESS_KEY_ID!,
